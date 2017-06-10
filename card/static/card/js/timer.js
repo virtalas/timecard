c8b82e297b395fe9e86a9ee303b123cbb7767916
virtalas <virtalas@users.noreply.github.com>
2017-06-10 14:39:25 +0300

    fixed stop for today button not appearing, added session length

--- card/static/card/js/timer.js
+++ card/static/card/js/timer.js
@@ -1,8 +1,10 @@
 var seconds_left = 0;
 var negative = false;
 var use_negative_sign = false; // used when timer turns negative on screen
+var startTime;
 
 $(document).ready(function(){
+  startTime = Date.parse($('#start_time').text());
   var hours = parseInt($('#hours').text());
   var minutes = parseInt($('#minutes').text());
 
@@ -36,4 +38,25 @@ function update_timer() {
   if (use_negative_sign) $('#hours').text("- " + hours); // used when timer turns negative on screen
   $('#minutes').text(minutes);
   $('#seconds').text(seconds);
+
+  // Update length
+  var length = Date.now() - startTime;
+  $('#length').text(msToTime(length));
+}
+
+function msToTime(s) {
+  var ms = s % 1000;
+  s = (s - ms) / 1000;
+  var secs = s % 60;
+  s = (s - secs) / 60;
+  var mins = s % 60;
+  var hrs = (s - mins) / 60;
+
+  var length = "";
+
+  if (hrs) length += hrs + " hours, ";
+  length += mins + " minutes, ";
+  length += secs + " seconds";
+
+  return length;
 }
